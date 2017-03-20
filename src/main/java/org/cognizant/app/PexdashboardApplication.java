17ca502c965ee2456a869fa56cace7760a853ef4
rmabini <mabini.roberto@gmail.com>
2017-03-20 19:14:10 +0800

    Baseline Initial

---
+++ src/main/java/org/cognizant/app/PexdashboardApplication.java
@@ -0,0 +1,12 @@
+package org.cognizant.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PexdashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PexdashboardApplication.class, args);
+	}
+}
